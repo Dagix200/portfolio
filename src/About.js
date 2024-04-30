a9bbccf6b24e8eac269a0f9c6738497729f4468e
Dagix200 <jarmuzra@gmail.com>
2024-04-30 23:39:11 +0200

    dodano animacje

--- src/About.js
+++ src/About.js
@@ -7,8 +7,8 @@ function About() {
                 <h2><span className='aboutUnderline'>Ab</span>out me</h2>
             </div>
             <div className='aboutMainContent'>
-                <img src='boss.jpg'/>
-                <p>My name is Damian. I've been a junior full stack developer for a year now. Continuously passionate about improving my skills, I continue to develop my potential in the world of full stack development, striving to create effective and user-friendly apps.</p>
+                <img src='boss.jpg' className='AboutIMG'/>
+                <p className='AboutP'>My name is Damian. I've been a junior full stack developer for a year now. Continuously passionate about improving my skills, I continue to develop my potential in the world of full stack development, striving to create effective and user-friendly apps.</p>
             </div>
         </div>
     )
--- src/App.js
+++ src/App.js
@@ -9,6 +9,22 @@ import Contacts from './Contacts';
 import Footer from './Footer';
 
 function App() {
+  window.addEventListener('scroll', function() {
+    var element1 = document.querySelector('.AboutP');
+    var element2 = document.querySelector('.AboutIMG');
+    var element3 = document.querySelector('.LangMainContent');
+    var element4 = document.querySelector('.SoftMainContent');
+
+    var elements = [element1, element2, element3, element4]
+    var className = ['animateAboutMainP', 'animateAboutMainIMG', 'animateLangMainContent', 'animateSoftMainContent']
+  
+    elements.forEach((element, index) => {
+      var position = element.getBoundingClientRect();
+      if(position.top >= 0 && position.bottom <= window.innerHeight) {
+        element.classList.add(className[index]);
+      }
+    })
+  });
   return (
     <div className="App">
       <Navbar/>
